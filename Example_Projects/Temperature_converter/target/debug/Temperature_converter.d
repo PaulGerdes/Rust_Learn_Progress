ec6d40f62667bcffff58b79df947a364d0057cd0
PaulGerdes <paul-gerds@gmx.net>
2024-11-04 20:49:24 +0100

    Example Project Temperature Converter fixing Temp values + loop labels

---
+++ Example_Projects/Temperature_converter/target/debug/Temperature_converter.d
@@ -0,0 +1,1 @@
+C:\Users\paul-\projects\RustLernen\Example_Projects\Temperature_converter\target\debug\Temperature_converter.exe: C:\Users\paul-\projects\RustLernen\Example_Projects\Temperature_converter\src\main.rs
---
+++ Example_Projects/Temperature_converter/target/debug/deps/Temperature_converter.d
@@ -0,0 +1,5 @@
+C:\Users\paul-\projects\RustLernen\Example_Projects\Temperature_converter\target\debug\deps\Temperature_converter.exe: src/main.rs
+
+C:\Users\paul-\projects\RustLernen\Example_Projects\Temperature_converter\target\debug\deps\Temperature_converter.d: src/main.rs
+
+src/main.rs:
