b9403b66269c99479e9933e6b977e20a80a54b84
Paul Gerdes <96906057+PaulGerdes@users.noreply.github.com>
2024-11-06 11:55:20 +0100

    fixes

--- Example_Projects/Temperature_converter/src/main.rs
+++ Example_Projects/Temperature_converter/src/main.rs
@@ -6,22 +6,16 @@ fn main() {
         let mut user_input_temp = String::new();
         println!("Bitte Temperatur eingeben");
         io::stdin()
-
             .read_line(&mut user_input_temp)
-
             .expect("Failed to read line");
         let user_input_temp: f64 = match user_input_temp.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
-
-
         let mut user_input = String::new();
         println!("Zwischen 3 Moeglichkeiten waehlen:\n1. Grad in Fahreneinheit\n2. Fahreneinheit in Grad");
         io::stdin()
-
             .read_line(&mut user_input)
-
             .expect("Failed to read line");
         let user_input: f64 = match user_input.trim().parse() {
             Ok(num) => num,
@@ -31,7 +25,6 @@ fn main() {
             println!("Bitte wähle eine Möglichkeit zwischen 1 und 3");
             continue;
         }
-
         if user_input == 1.0{
             println!("Grad zu Fahreneinheit wird gerechnet!");
             let result = {
@@ -40,7 +33,6 @@ fn main() {
             };
             println!("Das Ergebnis in Fahreneinheit ist: {}", result);
         }
-
         if user_input == 2.0{
             println!("Fahreneinheit zu Grad wird gerechnet!");
             let result = {
@@ -49,6 +41,4 @@ fn main() {
             println!("Das Ergebnis in Grad ist: {}", result);
         }
     }
-
-
 }
