cb7a6e39ec00efd935c4eca385c281aba7b8c36f
PaulGerdes <paul-gerds@gmx.net>
2024-11-06 19:31:10 +0100

    Adding References and Borrowing

---
+++ References_and_Borrowing/target/debug/References_and_Borrowing.d
@@ -0,0 +1,1 @@
+C:\Users\paul-\projects\RustLernen\References_and_Borrowing\target\debug\References_and_Borrowing.exe: C:\Users\paul-\projects\RustLernen\References_and_Borrowing\src\main.rs
---
+++ References_and_Borrowing/target/debug/deps/References_and_Borrowing.d
@@ -0,0 +1,5 @@
+C:\Users\paul-\projects\RustLernen\References_and_Borrowing\target\debug\deps\References_and_Borrowing.exe: src/main.rs
+
+C:\Users\paul-\projects\RustLernen\References_and_Borrowing\target\debug\deps\References_and_Borrowing.d: src/main.rs
+
+src/main.rs:
