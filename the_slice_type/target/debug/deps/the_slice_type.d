0a5edf5db2e20b46201ce1b70423af0a3e105efe
PaulGerdes <paul-gerds@gmx.net>
2025-01-03 00:57:29 +0100

    Adding the_slice_type

---
+++ the_slice_type/target/debug/deps/the_slice_type.d
@@ -0,0 +1,5 @@
+C:\Users\paul-\projects\RustLernen\the_slice_type\target\debug\deps\the_slice_type.exe: src/main.rs
+
+C:\Users\paul-\projects\RustLernen\the_slice_type\target\debug\deps\the_slice_type.d: src/main.rs
+
+src/main.rs:
