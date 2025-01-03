fdd79964337aebe50e4f4d64449451338d7e25b4
PaulGerdes <paul-gerds@gmx.net>
2025-01-03 02:36:03 +0100

    Added Defining_an_Enum

---
+++ Defining_an_Enum/target/debug/Defining_an_Enum.d
@@ -0,0 +1,1 @@
+C:\Users\paul-\projects\RustLernen\Defining_an_Enum\target\debug\Defining_an_Enum.exe: C:\Users\paul-\projects\RustLernen\Defining_an_Enum\src\main.rs
---
+++ Defining_an_Enum/target/debug/deps/Defining_an_Enum.d
@@ -0,0 +1,5 @@
+C:\Users\paul-\projects\RustLernen\Defining_an_Enum\target\debug\deps\Defining_an_Enum.exe: src/main.rs
+
+C:\Users\paul-\projects\RustLernen\Defining_an_Enum\target\debug\deps\Defining_an_Enum.d: src/main.rs
+
+src/main.rs:
