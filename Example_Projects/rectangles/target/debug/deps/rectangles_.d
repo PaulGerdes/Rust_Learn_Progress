0e43cc04d4ad110d11eca2f203e13154db3158e6
PaulGerdes <paul-gerds@gmx.net>
2025-01-03 01:58:00 +0100

    Added Method_Syntax

---
+++ Example_Projects/rectangles/target/debug/deps/rectangles_.d
@@ -0,0 +1,5 @@
+C:\Users\paul-\projects\RustLernen\Example_Projects\rectangles\target\debug\deps\rectangles_.exe: src/main.rs
+
+C:\Users\paul-\projects\RustLernen\Example_Projects\rectangles\target\debug\deps\rectangles_.d: src/main.rs
+
+src/main.rs:
---
+++ Example_Projects/rectangles/target/debug/rectangles_.d
@@ -0,0 +1,1 @@
+C:\Users\paul-\projects\RustLernen\Example_Projects\rectangles\target\debug\rectangles_.exe: C:\Users\paul-\projects\RustLernen\Example_Projects\rectangles\src\main.rs
