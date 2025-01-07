5699ed0cc1672232b3be6de0da5c8d18c340a05f
PaulGerdes <paul-gerds@gmx.net>
2025-01-07 17:38:03 +0100

    Added The match Control Flow Construct to Defining_an_Enum

--- Defining_an_Enum/src/main.rs
+++ Defining_an_Enum/src/main.rs
@@ -83,4 +83,86 @@ fn main() {
         Some(T),
     }
 
+
+
+    // The match control flow construct
+    enum Coin {
+        Penny,
+        Nickel,
+        Dime,
+        Quarter,
+    }
+    fn value_in_cents(value: i32) -> i32 {
+        match coin{
+            Coin::Penny => {
+                println!("Lucky penny!");
+                1
+            },
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter => 0,
+        }
+    }
+
+    // Paterns that bind to Values
+    #[derive(Debug)] // so we can inspect the state in a minute
+    enum UsState {
+        Alabama,
+        Alaska,
+        // --snip--
+    }
+    enum Coin2 {
+        Penny,
+        Nickel,
+        Dime,
+        Quarter(UsState),
+    }
+    fn value_in_cents2(coin: Coin) -> u8 {
+        match coin {
+            Coin::Penny => 1,
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin2::Quarter(state) => {
+                println!("State quarter from {state:?}!");
+                25
+            }
+        }
+    }
+
+
+
+    // Matching with Option<T>
+
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+
+
+
+    // Catch-all Patterns and the _ Placeholder
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        other => move_player(other),
+        _ => reroll(),
+        _ => (),
+        // 3 different types the underline means that we dont use any other values than the used ones
+        // in the top
+    }
+
+    fn add_fancy_hat() {}
+    fn remove_fancy_hat() {}
+    fn move_player(num_spaces: u8) {}
+
 }
+
+
